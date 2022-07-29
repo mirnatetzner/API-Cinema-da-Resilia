b983c9de84d8b6dc38960bde135929bf6191647f
alicebranq <branq.alice@gmail.com>
2022-07-29 16:51:46 -0300

    feat: adição packagejson + inserção do código no app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import Combos from "./src/controllers/Combos.js"
+
+dotenv.config()
+
+const port = process.env.PORT || 3000;
+const app = express()
+
+/**
+ * verificação de servidor online
+ */
+    app.listen(port, () => {
+        console.log(`servidor online em http://localhost:${port}`)
+    })
+
+app.use(express.json())
+
+Combos.tiposdecombos(app)
