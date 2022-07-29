a2cd4019e03a7c4d646c2f60de5710491153bd91
mirnatetzner <mirnatetzner@gmail.com>
2022-07-29 17:47:18 -0300

    validation cadeiras comuns

--- src/services/ValidacoesSala.js
+++ src/services/ValidacoesSala.js
@@ -1,28 +1,52 @@
 import ValidacoesService from "./ValidacoesService.js";
 
-class ValidacoesSala extends ValidacoesService{
+class ValidacoesSala extends ValidacoesService {
 
-    static validaCadeiras_comuns(){
-
+    static validaCadeiras_comuns(cComum) {
+        if (!cComum === Boolean) {
+            cComum = Number(cComum)
+            console.log(cComum);
+            let Valid = Number.isInteger(cComum)
+            console.log(Valid);
+            if (Valid && cComum > 0) {
+                return true;
+            }
+        }
+        return false;
     }
-    static validaCadeiras_namoradeiras(){
 
+    static validaCadeiras_namoradeiras(cNamo) {
+        Number(cNamo)
+        if ( == NaN) {
+            return false;
+        }
+        if (Number.isInteger())
     }
-    static validaEspaços_cadeirantes(){
+    static validaEspaços_cadeirantes(cAcess) {
+        Number(cAcess)
+        if ( == NaN) {
+            return false;
+        }
+        if (Number.isInteger())
+    }
 
-    }
     /**
      * 
      * @param {boolean} cetificado 
      * @returns boolean
      */
-    static validaCertificado_de_vistoria_anual(certificado){
-        if(certificado != 1 || 0){
+    static validaCertificado_de_vistoria_anual(certificado) {
+        if (certificado != 1 || 0) {
             return false
         }
-    } 
+    }
 
-    static validaCategoria_da_sala(tipo){
+    /**
+     * 
+     * @param {string} tipo
+     * @returns boolean 
+     */
+    static validaCategoria_da_sala(tipo) {
         switch (tipo) {
             case 'imax':
             case 'xd':
@@ -32,16 +56,13 @@ class ValidacoesSala extends ValidacoesService{
             case 'reald':
             case 'd-box':
                 true
-              break;
+                break;
             default:
-              console.log(`Você deve escolher uma das opções: imax, xd, macro xe, 4dx, xplus, reald e d-box (certifique-se de escrever como no exemplo). `);
-          }
+                console.log(`Você deve escolher uma das opções: imax, xd, macro xe, 4dx, xplus, reald e d-box (certifique-se de escrever como no exemplo). `);
+        }
     }
 
 
-
-    
-    
     /**
      * 
      * @param {}  
@@ -49,26 +70,25 @@ class ValidacoesSala extends ValidacoesService{
      * @param {}  
      * @returns boolean
      */
-    static isValid(id, cadeiras_comuns, cadeiras_namoradeiras, espaços_cadeirantes, certificado_de_vistoria_anual, categoria_da_sala){
-        return this.validaDimensao() && this.validaCapacidade(telefone) && this.validaLucratividade() && this.validaExibicoes()
+    static isValid(id, cadeiras_comuns, cadeiras_namoradeiras, espaços_cadeirantes, certificado_de_vistoria_anual, categoria_da_sala) {
+        return this.validaCadeiras_comuns() && this.validaCadeiras_namoradeiras() && this.validaEspaços_cadeirantes() && this.validaCertificado_de_vistoria_anual(certificado) && this.validaCategoria_da_sala(tipo) {}
+
     }
-    
-}
 
-  /*  static validaLucratividade(valor){
-       let RegExp = /[,|.|\$|R]/g 
-       let valorClean = valor.replaceAll(RegExp,"")
-       valorfloat = parseFloat(valorClean)
-       if(valorfloat != Number){
-           return false
-       }
-           return true
-   }
+    /*  static validaLucratividade(valor){
+         let RegExp = /[,|.|\$|R]/g 
+         let valorClean = valor.replaceAll(RegExp,"")
+         valorfloat = parseFloat(valorClean)
+         if(valorfloat != Number){
+             return false
+         }
+             return true
+     }
 
 
-   static validaExibicoes(){
-       return 
-   }
-    */
+     static validaExibicoes(){
+         return 
+     }
+      */
 
-export default ValidacoesSala
+    export default ValidacoesSala
