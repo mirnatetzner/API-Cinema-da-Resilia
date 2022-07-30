e3d8f9d772765279ed43f5888328259a9b19c878
alicebranq <branq.alice@gmail.com>
2022-07-30 11:29:28 -0300

    feat: arquivo de população da tabela  + ajuste em CombosModels

--- src/infra/CriaEPopula.js
+++ src/infra/CriaEPopula.js
@@ -1,1 +1,20 @@
 import CombosDatabase from "./CombosDatabase.js";
+
+const combos = {
+    name: "",
+    price: "",
+    item1: "", 
+    item2: "", 
+    item3: ""
+}
+try {
+    const criaCombos = await CombosDatabase.createTableCombos()
+    console.log(criaCombos)
+
+    const addNovoCombo = await CombosDatabase.adicaoNovosCombos()
+    console.log(addNovoCombo)
+
+} catch(e) {
+    console.log(e)
+
+}
--- src/models/CombosModels.js
+++ src/models/CombosModels.js
@@ -2,7 +2,7 @@ class CombosModels{
     constructor(name, price, payment, item1, item2, item3) {
         this.name = name
         this.price = price
-        this.payment = payment
+        // this.payment = payment
         this.item1 = item1
         this.item2 = item2
         this.item3 = item3
