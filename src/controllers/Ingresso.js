1f965a0d9991735dc3e21b389902879c784ac41e
joaofmds <ms.joao.felipe@gmail.com>
2022-08-02 15:33:13 -0300

    Criação dos métodos de patch e delete em Ingresso.js

--- src/controllers/Ingresso.js
+++ src/controllers/Ingresso.js
@@ -4,47 +4,67 @@ import IngressoMetodos from "../DAO/IngressoMetodos.js"
 
 class Ingresso {
     static rotas(app) {
+        app.get('/ingresso/', async (req, res) => {
+            try {
+                const ingresso = await IngressoMetodos.listarTodosIngressos()
+                res.status(200).json(ingresso)
+            } catch (error) {
+                res.status(500).json(error.message)
+            }
+        })
+
         app.get('/ingresso/:id', async (req, res) => {
             try {
                 const ingresso = await IngressoMetodos.listarIngressoPorId(req.params.id)
                 res.status(200).json(ingresso)
-            } catch(erro) {
-                res.status(500).json(erro.message)
+            } catch(error) {
+                res.status(500).json(error.message)
             } 
         })
             
         app.post('/ingresso/:id', async (req, res) => {
-            const ingresso = req.body
             try {
-            const result = await IngressoMetodos.inserirIngresso(ingresso)
-                res.status(201).json({ msg: result })
+            const ingresso = await IngressoMetodos.inserirIngresso(ingresso)
+                res.status(201).json(ingresso)
             } catch (error) {
-                res.status(500).json({ erro: error })
+                res.status(500).json(error.message)
             }
 
         })
 
         app.patch('/ingresso/:id', async (req, res) => {
             const id = req.params.id
-            const ingresso = req.body
 
             try {
                 const ingresso = await IngressoMetodos.listarIngressoPorId(id)
 
                 if (ingresso) {
-                    const result = await IngressoMetodos.atualizarIngressoPorId(id, ingresso)
-                    res.status(200).json({ msg: result })
+                    const ingresso = await IngressoMetodos.atualizarIngressoPorId(id, ingresso)
+                    res.status(200).json(ingresso)
                 } else {
                     res.status(404).json({msg: 'ingresso não encontrado.'})
                 }
             } catch (error) {
-                res.status(500).json({ erro: error })
+                res.status(500).json(error.message)
             }
             })
 
-        app.delete('/ingresso/id', (req, res) => {
-            
-        })
+        app.delete('/ingresso/:id', async(req, res) => {
+                const id = req.params.id
+        
+                try {
+                    const ingresso = await IngressoMetodos.listarIngressoPorId(id)
+        
+                    if (ingresso) {
+                        const ingresso = await IngressoMetodos.deletarIngressoPorId(id)
+                        res.status(200).json(ingresso)
+                    } else {
+                        res.status(404).json({msg: 'Ingresso não encontrado.'})
+                    }
+                } catch (error) {
+                    res.status(500).json(error.message)
+                }
+        })  
     }
 }
 
