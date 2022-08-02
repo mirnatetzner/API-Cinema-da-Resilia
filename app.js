820614fe9e6968f92444ccec935149478a6a2c78
joaofmds <95502785+joaofmds@users.noreply.github.com>
2022-08-02 01:10:39 -0300

    Sincronização e resolução de conflitos com base na develop (#4)
    * Sincronização e resolução de conflitos com base na develop
    
    * Alterações no controller: introdução do método async no controller Ingresso.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import Combos from "./src/controllers/Combos.js"
 import Salas from "./src/controllers/Sala.js";
 import Filmes from "./src/controllers/Filmes.js"
 import FilmesMetodos from "./src/DAO/FilmesMetodos.js";
+import Ingresso from "./src/controllers/Ingresso.js"
 
 dotenv.config()
 
@@ -24,4 +25,4 @@ Combos.combos(app)
 Salas.rotas(app)
 FilmesMetodos.createTableFilmes()
 Filmes.rotas(app)
-
+Ingresso.rotas(app)
