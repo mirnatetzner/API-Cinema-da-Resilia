37e1f117abbfe91455b69fd02c37b2b07d8c5272
joaofmds <ms.joao.felipe@gmail.com>
2022-08-03 17:08:01 -0300

    Atualizações do arquivo ValidacoesIngresso.js: inclusão de isValid

--- src/services/ValidacoesIngresso.js
+++ src/services/ValidacoesIngresso.js
@@ -1,35 +1,58 @@
 import ValidacoesService from "./ValidacoesService.js";
 export default class ValidacoesIngresso {
 
+    /**
+     * @param {string} filme 
+     * @returns boolean
+     */
     static validaFilme(filme) {
         return filme >= 2
     }
 
+    /**
+     * @param {number} sala 
+     * @returns boolean
+     */
     static validaSala(sala) {
-        sala >= 1&& sala <= 10
+        return sala >= 1
     }
 
+    /**
+     * @param {number} cadeira 
+     * @returns boolean
+     */
     static validaCadeira(cadeira) {
-        if (typeof cadeira != "boolean" ) {
-            const number = Number(cadeira)
-            let Valid = Number.isInteger(number)
-            if (Valid && number > 0) {
-                return true;
-            }
-        }
-        return false;
+        return ValidacoesService.validaNumerosInteiros(cadeira);
     }
 
+    /**
+     * @param {string} dataHora 
+     * @returns boolean
+     */
     static validaDataHora(dataHora) {
         const regex = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$^([01]?[0-9]|2[0-3]):[0-5][0-9]$/
         return regex.test(dataHora)
     }   
 
+    /**
+     * @param {number} precoIngresso 
+     * @returns boolean
+     */
     static validaPrecoIngresso(precoIngresso) {
-        return parseFloat(precoIngresso)
+        const preco = parseInt(precoIngresso)
+        return preco == precoIngresso
     }
 
-    static validaCodigoDeBarra(codigoDeBarra) {
-        return codigoDeBarra.length = 12
+     /**
+     * 
+     * @param {string} filme 
+     * @param {number} sala 
+     * @param {number} cadeira 
+     * @param {string} dataHora 
+     * @param {number} precoIngresso 
+     * @returns boolean
+     */
+      static isValid(filme, sala, cadeira, dataHora, precoIngresso){
+        return this.validaFilme(filme) && this.validaSala(sala) && this.validaCadeira(cadeira) && this.validaDataHora(dataHora) && this.validaPrecoIngresso(precoIngresso)
     }
 }
